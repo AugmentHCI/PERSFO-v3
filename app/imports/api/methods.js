4158da1b5de6afde4dfc76e08a5f3654e5e1cbce
Robin De Croon <robin.decroon@kuleuven.be>
2022-02-21 09:05:18 +0100

    new menu

--- app/imports/api/methods.js
+++ app/imports/api/methods.js
@@ -4,5 +4,5 @@ export const OpenSettings = new ReactiveVar(false);
 export const OpenFeedback = new ReactiveVar(false);
 export const OpenRecommenderExplanations = new ReactiveVar(null);
 
-export const FALLBACK_DATE = "2022-02-14";
-export const LAST_MENU_UPDATE = "2022-02-12_1";
+export const FALLBACK_DATE = "2022-02-21";
+export const LAST_MENU_UPDATE = "2022-02-21_1";
